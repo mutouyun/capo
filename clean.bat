856515e46bd3144606e4279c12c264e5c69ddad4
mutouyun <mark.lonr@tom.com>
2014-05-31 12:58:27 +0900

    修改clean.bat

--- clean.bat
+++ clean.bat
@@ -1,7 +1,14 @@
 @echo off
 
-rd /q /s "build*"
-rd /q /s "test\Debug"
-rd /q /s "test\Release"
+for /r %%a in (.) do (
+set list=%%a
+setlocal enabledelayedexpansion
+if "!list:~-8,6!"=="-Debug" rd /q /s "!list:~0,-2!"
+if "!list:~-10,8!"=="-Release" rd /q /s "!list:~0,-2!"
+endlocal
+)
+
+rd /q /s "test\debug"
+rd /q /s "test\release"
 
 del /f /q /s /a *.user *.ncb *.suo *.sdf *.i
