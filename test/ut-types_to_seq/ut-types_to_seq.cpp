330df09eabd1902f6218d08f77167034f15cd5f4
mutouyun <mark.lonr@tom.com>
2014-12-02 20:18:00 +0800

    完成test_type.h中ut用例的迁移工作

---
+++ test/ut-types_to_seq/ut-types_to_seq.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+
+#include "gtest/gtest.h"
+
+#include "capo/type_name.hpp"
+#include "capo/type_list.hpp"
+#include "capo/types_to_seq.hpp"
+
+////////////////////////////////////////////////////////////////
+
+#define TEST_METHOD(TEST_NAME) TEST(types_to_seq, TEST_NAME)
+
+////////////////////////////////////////////////////////////////
+
+TEST_METHOD(types_to_seq)
+{
+    EXPECT_EQ((capo::type_name<capo::constant_seq<0, 1, 2>>()), 
+              (capo::type_name<capo::types_to_seq<int, long, double>>()));
+}
+
+TEST_METHOD(list_to_seq)
+{
+    EXPECT_EQ((capo::type_name<capo::constant_seq<0, 1, 2>>()), 
+              (capo::type_name<capo::list_to_seq<capo::types<int, long, double>>>()));
+}
