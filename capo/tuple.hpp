eea55a412e3ff6dda3dce7137e7c6c2e34a78ab8
mutouyun <memleak@darkc.at>
2015-10-18 12:43:16 +0900

    简化代码

--- capo/tuple.hpp
+++ capo/tuple.hpp
@@ -27,17 +27,16 @@ namespace detail_forward_as_tuple_ {
 template <int... M, typename... A>
 std::tuple<> impl_(constant_seq<>, constant_seq<M...>, A&&...) { return {}; }
 
-template <int N1, int... N, int M1, int... M, typename A1, typename... A,
-          typename = typename std::enable_if<(sizeof...(N) < sizeof...(M))>::type>
-auto impl_(constant_seq<N1, N...>, constant_seq<M1, M...>, A1&& a, A&&... args)
+template <int N1, int... N, typename A1, typename... A,
+          typename = typename std::enable_if<(sizeof...(N) < sizeof...(A))>::type>
+auto impl_(constant_seq<N1, N...>, A1&& a, A&&... args)
 {
-    return std::tuple_cat(std::forward_as_tuple(std::forward<A1>(a)), 
-                          impl_(constant_seq<N...>{}, constant_seq<M...>{}, std::forward<A>(args)...));
+    return std::tuple_cat(std::forward_as_tuple(std::forward<A1>(a)), impl_(constant_seq<N...>{}, std::forward<A>(args)...));
 }
 
-template <int... N, int... M, typename... A,
-          typename = typename std::enable_if<(sizeof...(N) == sizeof...(M))>::type>
-auto impl_(constant_seq<N...>, constant_seq<M...>, A&&... args)
+template <int... N, typename... A,
+          typename = typename std::enable_if<(sizeof...(N) == sizeof...(A))>::type>
+auto impl_(constant_seq<N...>, A&&... args)
 {
     return std::forward_as_tuple(std::forward<A>(args)...);
 }
@@ -47,7 +46,7 @@ auto impl_(constant_seq<N...>, constant_seq<M...>, A&&... args)
 template <size_t ForwardN, typename... A>
 auto forward_as_tuple(A&&... args)
 {
-    return detail_forward_as_tuple_::impl_(size_to_seq<ForwardN>{}, types_to_seq<A...>{}, std::forward<A>(args)...);
+    return detail_forward_as_tuple_::impl_(size_to_seq<ForwardN>{}, std::forward<A>(args)...);
 }
 
 /*
