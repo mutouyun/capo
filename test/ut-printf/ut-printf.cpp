8e23dc8c0085470ab0459e9cd59a0f2b703f8a8b
mutouyun <mark.lonr@tom.com>
2015-10-27 13:50:39 +0900

    Try to locate the problem.

--- test/ut-printf/ut-printf.cpp
+++ test/ut-printf/ut-printf.cpp
@@ -20,17 +20,20 @@ int printf_test(const char* fmt, ...)
 {
     va_list args, args2, args3;
     va_start(args, fmt);
-    args3 = args2 = args;
+    va_start(args2, fmt);
     char* buf = nullptr; const char * s = nullptr; char c = '\0';
-    s = va_arg(args3, const char *);
-    c = va_arg(args3, char);
+    s = va_arg(args2, const char *);
+    c = va_arg(args2, char);
     printf("XXXXXXXXXXXXXXXXX -- 0: {%s}, {%c}\n", s, c);
     int n = ::vsnprintf(nullptr, 0, fmt, args);
-    printf("XXXXXXXXXXXXXXXXX -- 1: %d 0x%016x 0x%016x\n", n, (size_t)args, (size_t)args2);
+    printf("XXXXXXXXXXXXXXXXX -- 1: %d\n", n);
     if (n <= 0) goto exit_output;
     buf = new char[++n];
     n = ::vsnprintf(buf, n, fmt, args);
     printf("XXXXXXXXXXXXXXXXX -- 2: %d, %s\n", n, buf);
+    s = va_arg(args, const char *);
+    c = va_arg(args, char);
+    printf("XXXXXXXXXXXXXXXXX -- 3: {%s}, {%c}\n", s, c);
     if (n <= 0) goto exit_output;
     std::cout << buf;
 exit_output:
