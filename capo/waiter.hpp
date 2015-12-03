d8c272808c1ee0108755cea276e5aea810298eef
mutouyun <mark.lonr@tom.com>
2015-12-03 14:02:05 +0800

    调整命名

--- capo/waiter.hpp
+++ capo/waiter.hpp
@@ -18,9 +18,9 @@ namespace capo {
 // names for waiter status
 enum class waiter_status
 {
-    resting,
-    arrived,
-    excited
+    Resting,
+    Arrived,
+    Excited
 };
 
 ////////////////////////////////////////////////////////////////
@@ -31,7 +31,7 @@ class waiter
 {
     mutable std::mutex      lock_;
     std::condition_variable cond_;
-    waiter_status signaled_ = waiter_status::resting;
+    waiter_status signaled_ = waiter_status::Resting;
 
 public:
     waiter_status status(void) const
@@ -43,35 +43,35 @@ public:
     bool is_signaled(void) const
     {
         auto lc = capo::make<std::unique_lock>(lock_);
-        return (signaled_ != waiter_status::resting);
+        return (signaled_ != waiter_status::Resting);
     }
 
     void reset(void)
     {
         auto lc = capo::make<std::unique_lock>(lock_);
-        signaled_ = waiter_status::resting;
+        signaled_ = waiter_status::Resting;
     }
 
 public:
     void wait(void)
     {
         auto lc = capo::make<std::unique_lock>(lock_);
-        cond_.wait(lc, [this]{ return (signaled_ != waiter_status::resting); });
-        if (signaled_ == waiter_status::arrived)
-            signaled_ = waiter_status::resting;
+        cond_.wait(lc, [this]{ return (signaled_ != waiter_status::Resting); });
+        if (signaled_ == waiter_status::Arrived)
+            signaled_ = waiter_status::Resting;
     }
 
     template <typename Rep, typename Period>
     std::cv_status wait_until(const std::chrono::duration<Rep, Period>& abs_time)
     {
         auto lc = capo::make<std::unique_lock>(lock_);
-        while (signaled_ == waiter_status::resting)
+        while (signaled_ == waiter_status::Resting)
         {
             if (cond_.wait_until(lc, abs_time) == std::cv_status::timeout)
                 return std::cv_status::timeout;
         }
-        if (signaled_ == waiter_status::arrived)
-            signaled_ = waiter_status::resting;
+        if (signaled_ == waiter_status::Arrived)
+            signaled_ = waiter_status::Resting;
         return std::cv_status::no_timeout;
     }
 
@@ -84,14 +84,14 @@ public:
     void notify_one(void)
     {
         auto lc = capo::make<std::unique_lock>(lock_);
-        signaled_ = waiter_status::arrived;
+        signaled_ = waiter_status::Arrived;
         cond_.notify_all();
     }
 
     void notify_all(void)
     {
         auto lc = capo::make<std::unique_lock>(lock_);
-        signaled_ = waiter_status::excited;
+        signaled_ = waiter_status::Excited;
         cond_.notify_all();
     }
 };
